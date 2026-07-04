b5b1d0f473606b2f47ce58993c96bd55e31901b5
Sorin <soringoredev@gmail.com>
2026-07-04 11:52:30 +0300

    Populate CoffeeTest class

--- src/CoffeeTest.java
+++ src/CoffeeTest.java
@@ -1,2 +1,26 @@
 public class CoffeeTest {
+    public static void main(String[] args) {
+
+        CoffeeMaker mk = new CoffeeMaker();
+        DrinkCoffee d = new DrinkCoffee();
+
+        for (int i = 0; i < 10; i++) {
+            Coffee coffee = mk.makeCoffee();
+
+            System.out.println();
+
+            try {
+                d.drinkCoffee(coffee);
+            } catch (TemperatureException e) {
+                System.out.println("Exceptie T: " + e.getMessage() + "\ntemperatura: " + e.getT());
+            } catch (ConcentrationException e) {
+                System.out.println("Exceptie C: " + e.getMessage() + "\nconcentratia: " + e.getC());
+            }
+            finally {
+                System.out.println("Aparatul de cafea s-a oprit.");
+            }
+
+        }
+
+    }
 }
