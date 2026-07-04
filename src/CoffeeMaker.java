a9a174648d8da49b1fae2440d84c79ef5b2aaa58
Sorin <soringoredev@gmail.com>
2026-07-04 11:27:01 +0300

    Populate CoffeeMaker class

--- src/CoffeeMaker.java
+++ src/CoffeeMaker.java
@@ -1,2 +1,10 @@
 public class CoffeeMaker {
+
+    Coffee makeCoffee() {
+        System.out.println("Cafeaua se prepara ...");
+        int t = (int) (Math.random() * 100);
+        int c = (int) (Math.random() * 100);
+        Coffee coffee = new Coffee(t, c);
+        return coffee;
+    }
 }
