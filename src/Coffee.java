d85520c85a471dee7a7cab70a1c48f5f7e17c46e
Sorin <soringoredev@gmail.com>
2026-07-04 11:23:10 +0300

    Populate Coffee class

--- src/Coffee.java
+++ src/Coffee.java
@@ -1,2 +1,29 @@
 public class Coffee {
+    // props
+    private int temp;
+    private int conc;
+
+    //constructor
+    public Coffee(int temp, int conc) {
+        this.temp = temp;
+        this.conc = conc;
+    }
+
+    Coffee() {}
+
+    // getters
+
+
+    public int getTemp() {
+        return temp;
+    }
+
+    public int getConc() {
+        return conc;
+    }
+
+    @Override
+    public String toString() {
+        return "[temperatura cafelei = " + temp + " : concentratia = " + conc + "]";
+    }
 }
