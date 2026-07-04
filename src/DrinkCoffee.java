58bc9d6f2441f3acf57764a98f80d68786e678a0
Sorin <soringoredev@gmail.com>
2026-07-04 11:41:12 +0300

    Populate DrinkCoffee class

--- src/DrinkCoffee.java
+++ src/DrinkCoffee.java
@@ -1,2 +1,19 @@
 public class DrinkCoffee {
+
+    void drinkCoffee(Coffee coffee) throws TemperatureException, ConcentrationException {
+        if(coffee.getTemp() > 65) {
+            throw new TemperatureException(coffee.getTemp(), "Cafeaua e prea fierbinte. O aruncam!");
+        } else {
+            System.out.println("t: " + coffee.getTemp());
+        }
+
+        if(coffee.getConc() > 55) {
+            throw new ConcentrationException(coffee.getConc(), "Concentratie mare. O aruncam!");
+        } else {
+            System.out.println("c: " + coffee.getConc());
+        }
+
+
+    }
+
 }
