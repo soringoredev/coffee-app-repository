03c0694dd303d1040ce49c3b2dd08410a9177100
Sorin <soringoredev@gmail.com>
2026-07-04 11:36:13 +0300

    Populate ConcentrationException

--- src/ConcentrationException.java
+++ src/ConcentrationException.java
@@ -1,2 +1,13 @@
 public class ConcentrationException extends Exception {
+    // props
+    int c;
+
+    public ConcentrationException(int c, String msg) {
+        super(msg);
+        this.c = c;
+    }
+
+    public int getC() {
+        return c;
+    }
 }
